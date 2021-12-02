2a60098a773cb3d8820defad3f0c1f9fd9a4c69f
Evan Suma Rosenberg <suma@umn.edu>
2021-12-02 15:40:05 -0600

    teleportation removed

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@
  */ 
 
 import { Engine, Scene, UniversalCamera, Logger } from "@babylonjs/core";
-import { Vector3, Color3, Ray, Quaternion, Axis } from "@babylonjs/core";
-import { HemisphericLight, DirectionalLight } from "@babylonjs/core";
-import { AssetsManager, AbstractMesh, MeshBuilder, LinesMesh } from "@babylonjs/core";
+import { Vector3, Color3, Quaternion, Axis } from "@babylonjs/core";
+import { HemisphericLight, DirectionalLight, ShadowGenerator } from "@babylonjs/core";
+import { AssetsManager, AbstractMesh, StandardMaterial } from "@babylonjs/core";
 import { WebXRCamera, WebXRInputSource, WebXRControllerComponent } from "@babylonjs/core";
 
 // Side effects
@@ -16,11 +16,6 @@ import "@babylonjs/core/Helpers/sceneHelpers";
 import "@babylonjs/core/Materials/standardMaterial"
 import "@babylonjs/inspector";
 
-enum LocomotionMode 
-{
-    viewDirected,
-    teleportation
-}
 
 class Game 
 { 
@@ -34,11 +29,6 @@ class Game
 
     private rightGrabbedObject: AbstractMesh | null;
     private grabbableObjects: Array<AbstractMesh>;
-
-    private locomotionMode: LocomotionMode;
-    private laserPointer: LinesMesh | null;
-    private groundMeshes: Array<AbstractMesh>;
-    private teleportPoint: Vector3 | null;
 
     constructor()
     {
@@ -59,12 +49,6 @@ class Game
         // Variables for grabbing objects
         this.rightGrabbedObject = null;
         this.grabbableObjects = [];
-
-        // Initialize locomotion
-        this.locomotionMode = LocomotionMode.viewDirected;
-        this.laserPointer = null;
-        this.groundMeshes = [];
-        this.teleportPoint = null;  
     }
 
     start() : void 
@@ -100,21 +84,21 @@ class Game
        ambientlight.diffuse = new Color3(.25, .25, .25);
 
        // Add a directional light to imitate sunlight
-       var directionalLight = new DirectionalLight("sunlight", Vector3.Down(), this.scene);
+       var directionalLight = new DirectionalLight("sunlight", new Vector3(-1, -2, -1), this.scene);
+       directionalLight.position = new Vector3(0, 20, 0);
        directionalLight.intensity = 1.0;
+
+       // Create a shadow genrator
+       var shadowGenerator = new ShadowGenerator(1024, directionalLight);
+       shadowGenerator.usePercentageCloserFiltering = true;
+       shadowGenerator.setDarkness(0.4);
 
         // Creates a default skybox
         const environment = this.scene.createDefaultEnvironment({
-            createGround: true,
-            groundSize: 500,
-            groundColor: new Color3(0.69, 0.407, 0.203),
-            groundOpacity: 1,
+            createGround: false,
             skyboxSize: 750,
             skyboxColor: new Color3(.059, .663, .80)
-        }); 
-
-        // The ground should be selectable for teleportation
-        this.groundMeshes.push(environment!.ground!);
+        });
 
          // Creates the XR experience helper
          const xrHelper = await this.scene.createDefaultXRExperienceAsync({});
@@ -122,17 +106,6 @@ class Game
          // Remove default teleportation and pointer selection
         xrHelper.teleportation.dispose();
         xrHelper.pointerSelection.dispose();
-
-        // Create points for the laser pointer
-        var laserPoints = [];
-        laserPoints.push(new Vector3(0, 0, 0));
-        laserPoints.push(new Vector3(0, 0, 1));
-
-        // Create a laser pointer
-        this.laserPointer = MeshBuilder.CreateLines("laserPointer", {points: laserPoints}, this.scene);
-        this.laserPointer.color = Color3.White();
-        this.laserPointer.alpha = .5;
-        this.laserPointer.visibility = 0;
 
          // Assigns the web XR camera to a member variable
          this.xrCamera = xrHelper.baseExperience.camera;
@@ -147,20 +120,8 @@ class Game
             else 
             {
                 this.rightController = inputSource;
-                this.laserPointer!.parent = this.rightController.pointer;
             }  
         });
-
-        // Don't forget to deparent the laser pointer or it will be destroyed!
-        xrHelper.input.onControllerRemovedObservable.add((inputSource) => {
-
-            if(inputSource.uniqueId.endsWith("right")) 
-            {
-                this.laserPointer!.parent = null;
-                this.laserPointer!.visibility = 0;
-            }
-        });
-
         
          // The assets manager can be used to load multiple assets
          var assetsManager = new AssetsManager(this.scene);
@@ -180,14 +141,26 @@ class Game
              // Search through the loaded meshes
              worldTask.loadedMeshes.forEach((mesh) => 
              {
-                // Remove the ground from the model
+                // Find the ground in the original scene
                 if(mesh.name == "SM_Box_Ground_01")
                 {
-                    mesh.dispose();
+                    // Set a new material color
+                    var groundMaterial = new StandardMaterial("groundMaterial", this.scene);
+                    groundMaterial.diffuseColor = new Color3(.62, .514, .396);
+                    groundMaterial.specularColor = Color3.Black();
+                    mesh.material = groundMaterial;
+
+                    // The ground should receive shadows
+                    mesh.receiveShadows = true;
                 }
                 else 
                 {
+                    // Make the object grabbable
                     this.grabbableObjects.push(mesh);
+
+                    // Make the object cast a shadow
+                    shadowGenerator.addShadowCaster(mesh);
+
                 }  
                 
              });
@@ -346,15 +319,6 @@ class Game
             if(component?.pressed)
             {
                 Logger.Log("right A pressed");
-
-                if(this.locomotionMode == LocomotionMode.teleportation)
-                {
-                    this.locomotionMode = 0;
-                }
-                else
-                {
-                    this.locomotionMode += 1;
-                }
             }
             else
             {
@@ -392,70 +356,27 @@ class Game
             }
         }  
 
-        if(component?.changes.axes)
+        // Check to see if the controller is connected
+        if(component)
         {
-            Logger.Log("right thumbstick axes: (" + component.axes.x + "," + component.axes.y + ")");
+            // Get the current camera direction
+            var directionVector = this.xrCamera!.getDirection(Axis.Z);
 
-            // View-directed steering
-            if(this.locomotionMode == LocomotionMode.viewDirected)
-            {
-                // Get the current camera direction
-                var directionVector = this.xrCamera!.getDirection(Axis.Z);
+            // Use delta time to calculate the move distance based on speed of 3 m/sec
+            var moveDistance = -component!.axes.y * (this.engine.getDeltaTime() / 1000) * 3;
 
-                // Use delta time to calculate the move distance based on speed of 3 m/sec
-                var moveDistance = -component.axes.y * (this.engine.getDeltaTime() / 1000) * 3;
+            // Translate the camera forward
+            this.xrCamera!.position.addInPlace(directionVector.scale(moveDistance));
 
-                // Translate the camera forward
-                this.xrCamera!.position.addInPlace(directionVector.scale(moveDistance));
+            // Use delta time to calculate the turn angle based on speed of 60 degrees/sec
+            var turnAngle = component!.axes.x * (this.engine.getDeltaTime() / 1000) * 60;
 
-                // Use delta time to calculate the turn angle based on speed of 60 degrees/sec
-                var turnAngle = component.axes.x * (this.engine.getDeltaTime() / 1000) * 60;
-
-                // Smooth turning
-                var cameraRotation = Quaternion.FromEulerAngles(0, turnAngle * Math.PI / 180, 0);
-                this.xrCamera!.rotationQuaternion.multiplyInPlace(cameraRotation);
-            }
-            // Teleportation
-            else
-            {
-                // If the thumbstick is moved forward
-                if(component.axes.y < -.75)
-                {
-                    // Create a new ray cast
-                    var ray = new Ray(this.rightController!.pointer.position, this.rightController!.pointer.forward, 20);
-                    var pickInfo = this.scene.pickWithRay(ray);
-
-                    // If the ray cast intersected a ground mesh
-                    if(pickInfo?.hit && this.groundMeshes.includes(pickInfo.pickedMesh!))
-                    {
-                        this.teleportPoint = pickInfo.pickedPoint;
-                        this.laserPointer!.scaling.z = pickInfo.distance;
-                        this.laserPointer!.visibility = 1;
-                    }
-                    else
-                    {
-                        this.teleportPoint = null;
-                        this.laserPointer!.visibility = 0;
-                    }
-                }
-                // If thumbstick returns to the rest position
-                else if(component.axes.y == 0)
-                {
-                    this.laserPointer!.visibility = 0;
-
-                    // If we have a valid targer point, then teleport the user
-                    if(this.teleportPoint)
-                    {
-                        this.xrCamera!.position.x = this.teleportPoint.x;
-                        this.xrCamera!.position.y = this.teleportPoint.y + this.xrCamera!.realWorldHeight;
-                        this.xrCamera!.position.z = this.teleportPoint.z;
-                        this.teleportPoint = null;
-                    }
-                }
-            }
+            // Smooth turning
+            var cameraRotation = Quaternion.FromEulerAngles(0, turnAngle * Math.PI / 180, 0);
+            this.xrCamera!.rotationQuaternion.multiplyInPlace(cameraRotation);
         }
+ 
     }  
-
 }
 /******* End of the Game class ******/   
 
